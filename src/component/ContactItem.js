4f9c091dde6aa29f4e96a5961f7cc4e18992bfc4
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-26 10:26:53 +0900

    netlify build 에러 해결을 위한 img alt 속성 추가

--- src/component/ContactItem.js
+++ src/component/ContactItem.js
@@ -6,10 +6,10 @@ const ContactItem = () => {
   return (
    <Row>
         <Col lg={1}>
-            <img width={50} src="https://th.bing.com/th/id/OIP.BoqJ8tKGpWzJm8UbfGVbOwHaHa?w=211&h=212&c=7&r=0&o=5&pid=1.7"/>
+            <img width={50} src="https://th.bing.com/th/id/OIP.BoqJ8tKGpWzJm8UbfGVbOwHaHa?w=211&h=212&c=7&r=0&o=5&pid=1.7" alt="user-image"/>
         </Col>
         <Col lg={11}>
-            <div>최미영</div>
+            <div>홍길순</div>
             <div>01023456767</div>
         </Col>
    </Row>
