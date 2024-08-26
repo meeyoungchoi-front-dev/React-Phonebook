c2eef295941d519bfe17f82c822720c45ac68d60
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-26 11:28:37 +0900

    redux 사용해서 이름으로 검색 기능 개발

--- src/component/ContactList.js
+++ src/component/ContactList.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import SearchBox from './SearchBox'
-import ContactItem from './ContactItem'
+import React from 'react';
+import SearchBox from './SearchBox';
+import ContactItem from './ContactItem';
+import { useSelector } from 'react-redux';
 
 const ContactList = () => {
+  // 필터된 연락처 목록을 가져옴
+  const contactList = useSelector(state => state.filteredList);
+
   return (
     <div>
-      <SearchBox/>
-      <ContactItem/>
-      <ContactItem/>
-      <ContactItem/>
-      <ContactItem/>
+      <SearchBox />
+      {contactList.map((item, index) => (
+        <ContactItem 
+          key={index} 
+          item={item} 
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ContactList
+export default ContactList;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,33 @@
 let initialState = {
-    contactList: []
+    contactList: [],  // 초기 연락처 목록
+    filteredList: []  // 필터링된 연락처 목록
 };
-function reducer(state=initialState, action) {
-    const {type, payload} = action;
-    if (type === "ADD_CONTACT") {
-        return {...state, contactList:[...state.contactList, {name: payload.name, phoneNumber: payload.phoneNumber}]}
+
+function reducer(state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case "ADD_CONTACT":
+            const updatedContactList = [...state.contactList, { name: payload.name, phoneNumber: payload.phoneNumber }];
+            return {
+                ...state,
+                contactList: updatedContactList,
+                filteredList: updatedContactList // 새로운 연락처 추가 시 필터된 목록도 업데이트
+            };
+        
+        case "SEARCH_BY_USERNAME":
+            const keyword = payload.keyword.toLowerCase();
+            const filteredList = state.contactList.filter(contact =>
+                contact.name.toLowerCase().includes(keyword)
+            );
+            return {
+                ...state,
+                filteredList  // 검색 결과를 상태로 업데이트
+            };
+
+        default:
+            return state;
     }
-    return [...state];
-}   
+}
 
 export default reducer;
